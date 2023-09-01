4467927d70440cb0633cf59fff33387024cfe47e
wahyu <yohanesrestuwahyu@gmail.com>
2023-09-01 12:53:57 +0700

    Membuat Route Peminjaman Ruangan

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeminjamanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group(['prefix' => 'peminjaman'], function(){
+    Route::get('/', [PeminjamanController::class, 'index']);
+    Route::post('/', [PeminjamanController::class, 'store']);
+    Route::group(['prefix' => '{id_peminjam}'], function(){
+        Route::get('/', [PeminjamanController::class, 'destroy']);
+        Route::get('/', [PeminjamanController::class, 'update']);
+    });
 });
